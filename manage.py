bf099fd3d6b65326d432feccd3c9c91828993e71
yanxiliu <24305445@student.uwa.edu.au>
2025-05-15 11:27:02 +0800

    Refactor: remove redundant entry files and use manage.py as entry point

--- manage.py
+++ manage.py
@@ -6,6 +6,11 @@ from config import Config
 app = create_app(Config)
 migrate = Migrate(app, db)
 
+
 @app.shell_context_processor
 def make_shell_context():
     return {"db": db, "User": User, "Post": Post, "Notification": Notification}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
